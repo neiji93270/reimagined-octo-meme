babd2ceeb48215a62146d023c532547e731fcdca
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-23 12:13:08 +0200

    Create Targetpcbeviouronethatisstillattackedno'hypothesisdrawmatchheaders.h

---
+++ Targetpcbeviouronethatisstillattackedno'hypothesisdrawmatchheaders.h
@@ -0,0 +1,14 @@
+//the rainbow qttack should not be used. then, it may not need any hypothesis
+//like for dns tunneling. to keep track of a draw match
+//nevertheless, use rainbow attack tresholds because
+//any new quatum physics values are good to deal with this current problem.
+//the problem is then to prevent loosing bits that is the job of supercomputers this one too.
+//the patterns could be feynman motives on high energy systems.
+
+
+class betterfeynmannmotivestotracksupercomptingsupergpusongddrram.
+
+
+class responsestacksononcpucachetohdbitsthztarerecoredeonusbstick
+
+class nohypothesistwothenuseosschedullerfailedlowenergydeviceplugforfourprogramsfailuretoo
