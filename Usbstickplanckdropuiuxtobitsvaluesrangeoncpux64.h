490f0b337226175c526b8ab3d988c4be8d0baf2b
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-23 12:03:33 +0200

    Create Usbstickplanckdropuiuxtobitsvaluesrangeoncpux64.h

---
+++ Usbstickplanckdropuiuxtobitsvaluesrangeoncpux64.h
@@ -0,0 +1,7 @@
+//we need models and log function. the high energy systems could be solitted
+//we proof that sensitive datas on pc can use models too.
+//so we first need high mathematical computing library to solve low energies that
+//deqls with bits bigger than ciphers on usb com port.
+//we dont need to set any treshold but super computers are suited for it.
+
+class onsticksendlastuiuxcomputed...
