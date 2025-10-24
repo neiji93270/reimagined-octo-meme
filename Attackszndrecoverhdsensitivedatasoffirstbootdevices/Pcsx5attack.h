5c5f503a28d0b13292861cf27d7bbb3586522593
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-24 09:15:41 +0200

    Update Pcsx5attack.h

--- Attackszndrecoverhdsensitivedatasoffirstbootdevices/Pcsx5attack.h
+++ Attackszndrecoverhdsensitivedatasoffirstbootdevices/Pcsx5attack.h
@@ -1,1 +1,18 @@
+//cyber attack related ro better emulators than mine ones.
+//rhe usb stick can accept.hd hijacking.and the com with the supercomputer
+//dont rely on models. to drop pcsx5 source code to
+//enslave this full repository, we will first set other tresholds
+//for flusho'g.useless datas displayed by the gpus and gddrram.
+//those.tresholds.are.not.based on hikacking.gpus not.hijacking graphic libraries.
+//then, a shared pointer would be the way because in chaotic systems,
+//the behaviour od'the inputs in high energy devices.mah partiallynrely on 
+//topological anomalies in models. that said we would have only one.treshold.or.127^15 or 127^17 tresholds.
+//the rainbow attack.is.then.useless and we do't.wznr.to.use nrute force.
+//it is then best to rely on a good timesramp. lets then use 10^17 tresholds.i'.a.daranase.if
+//the datas in hd use latest.mean field.gzmes.thepries. so.there is an esuilibrium.a'd.we.could.drop the higher tresholds numbers if we can have a track of shared pointerd.stacks.
 
+
+//the ai bios.could help.and.ti'ier cpus could be better than larger.cpus.
+//the gpus so couldnt know.what cpunis rhe best if the motherboard.have muliiple.proxessors.
+//so.the cpres of.one cpu must have been overxlocked. 
+class firsrgpuatrackwithfpgabehaviournotresholdsyetbutaibios
