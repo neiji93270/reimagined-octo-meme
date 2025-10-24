397b6a0cfa768b1fbbc046b4e64d4afc6e223682
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-24 06:36:58 +0200

    Create Anonymoususbforcing.h

---
+++ Customcomfromanonymouscom/Anonymoususbforcing.h
@@ -0,0 +1,16 @@
+//instead of usi'g psychometric thinking,
+//we czn rely on models here : https://amzn.eu/d/5bA9slR
+//it then means that high energy sytels.will completely delete all.of.the usb bits.
+//tonprevent it, we can set three tresholds at each bits transfer.
+//this allow us to keep track of 17 or 12 stack trace because we deal with physics.
+//plus, plancks constant is preserved but would fail to choose an ai model in the sender messages.
+//so we have to send the chose' tresholds to super computer that sent the packers.
+//ghis means that the super compiter power supply must not force communication for 10^-15 MeV communications.
+
+
+//the shared pointers would be pointers better than linux kernel pointers.
+//and the number of max processes would be set to 10^15 which seems what linux kernel could accept.
+//then, to have a better overview of non subsystem process stacks, we would use three tresholds
+//the tresholds must not be a key concept to those models.
+//so use som toolbox.
+class enforcerelianilitybyrainbowattacktwiceandenforcrtargetsubsystemmodulardevicesbysharedpointers
